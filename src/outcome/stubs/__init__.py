c89c58f9c476fe5c05dcefb31a84098762fa7d44
Douglas Willcocks <douglas@d-tw.org>
2021-02-12 19:40:46 +0100

    ci: harmonise CI (#2)
    * feat: initial
    
    * build: fix filename
    
    * ci: harmonize ci

---
+++ src/outcome/stubs/__init__.py
@@ -0,0 +1,4 @@
+# This file is not packaged in the build, it exists to provide a
+# file to test for coverage purposes
+
+stubs = True
---
+++ test/test_stub.py
@@ -0,0 +1,6 @@
+from outcome.stubs import stubs
+
+
+# Dummy test for CI
+def test_stub():
+    assert stubs
